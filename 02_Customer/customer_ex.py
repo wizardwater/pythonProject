853201d59326831d3d536ae908f8f85f2ac27ee9
wizardwater <wizardwater@naver.com>
2024-03-12 16:32:50 +0900

    insert data 기능 추가

--- 02_Customer/customer_ex.py
+++ 02_Customer/customer_ex.py
@@ -1,4 +1,14 @@
-import re
+import re, pickle, json
+
+def load_data():
+    f = open('02_Customer/data.json','r')
+    return json.load(f)
+
+def save_data(custlist):
+    f = open("02_customer/data.json","w")
+    json.dump(custlist,f,indent=2)
+    f.close()
+
 
 def print_menu():
     print('''
@@ -19,18 +29,18 @@ def gender_():
             break
     return gender
 def email_():
+    regex = re.compile('^[a-z][a-z0-9]{4,20}@[a-z]{2,10}.[a-z]{2,10}')
     while True:
-        email = input("이메일을 입력해주세요")
-        conf =re.search('@',email)
-
+        email = input("이메일을 입력해주세요 :")
+        conf =regex.search(email)
         if conf:
             break
-        else: print("'@'를 포함하세요")
+        else: print("'@'를 포함하여 이메일 형식으로 입력하세요")
     return email
 
 def birth_():
     while True:
-        birth = input("생일을 4자리로 입력해주세요")
+        birth = input("출생년도를 4자리로 입력해주세요 :")
         if len(birth) == 4:
             break
     return birth
@@ -41,44 +51,43 @@ def new_customer():  # 성별 /이메일/ 생일 받기
     email = email_()
     birth = birth_()
 
-    customer= {"Gender" : gender, "Email" : email, "Birthday": birth}
+    customer= {"Gender" : gender, "Email" : email, "Birthyear": birth}
     custlist.append(customer)
     
+    
 
-
-def customer_info(x):   # 현재 고객 -> 마지막 고객을 보여줌. 그럼 다음 고객 정보는 의미없는거아냐?
+def customer_info(x):
+    global page  # 전역변수를 변경하는 것임을 공표
     if page >=0:
         if x == 1:
             print("현재 고객 정보 조회")
-            print(custlist[page])
-            print(f"현재 페이지는 {page+1} 입니다")
         if x == 2:
             print("이전 고객 정보 조회")
-            if page == 0:
-                print(custlist[page])
-            else: print(custlist[page-1])
+            if page !=0:
+                page -= 1 
         if x == 3:
             print("다음 고객 정보 조회")
-            apa = 0
-            if page == page:
-                apa = -1
-            print(custlist[page+1+apa])
+            if page < len(custlist)-1:  # 고객3명일때 > 2 page =2
+                page +=1
+        print(custlist[page])
+        print(f"현재 페이지는 {page+1} 입니다")
 
-    else : print("아직 고객 정보가 없습니다")
+    else:
+        print("아직 고객 정보가 없습니다")
 
-def ex_amend():
+def ex_amend(idx):
     while True:
         choice=input('''
             그 외에 수정할 정보를 선택해 주세요 :
             G : 성별 수정
-            B : 생년월일 수정
+            B : 출생년도 수정
             Exit : 프로그램 종료 ''').upper()
         
         if choice=="G":
             custlist[idx]['Gender']=gender_()
                     
         elif choice=="B":
-            custlist[idx]['Birthday']=birth_()
+            custlist[idx]['Birthyear']=birth_()
                         
         elif choice=="EXIT":
             print("수정창 종료")
@@ -96,7 +105,7 @@ def amend_customer():
     else:
         email = email_()
         custlist[idx]['Email'] = email
-        ex_amend()
+        ex_amend(idx)
 
 
 def del_customer():
@@ -115,16 +124,17 @@ def del_customer():
         else: page = len(custlist) -1
 
 # 계속 실행할 while 문
-
 page = -1
-custlist = []
+custlist=load_data()
+page = len(custlist)-1
 cmd = 0
 while cmd != 'Q':
     print_menu()
     cmd = input().upper()
     if cmd == 'I':
         new_customer()  # 성별 /이메일/ 생년월일 받기
-        page += 1
+        page =len(custlist)-1
+        print(page)
     elif cmd == 'C':
         customer_info(1)
     elif cmd == 'P':
